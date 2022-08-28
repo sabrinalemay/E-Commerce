505a3481e19599c2d90a483986e7019d6a051fa7
Sabrina LeMay <b33na1997@gmail.com>
2022-08-27 22:46:04 -0700

    added products, categories, tags

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  forignKey: 'category_id'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  forignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  forignKey: 'prduct_id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  forignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
@@ -18,3 +29,7 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
+
+// git push this (remove this comment lol)
