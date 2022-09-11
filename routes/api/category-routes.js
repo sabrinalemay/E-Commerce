93d61309f0a005d4886e80a68d268c6448d7da61
Sabrina LeMay <b33na1997@gmail.com>
2022-09-11 12:11:34 -0700

    added routes to catergoryroutes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,20 +25,62 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findOne
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-  Category.create
+  router.post('/', (req, res) => {
+    // create a new category
+    Category.create({
+      category_name: req.body.category_name
+    })
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+  });
+
+  router.put('/:id', (req, res) => {
+    // update a category by its `id` value
+    Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+  router.delete('/:id', (req, res) => {
+    // delete a category by its `id` value
+    Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+  
 
-module.exports = router;
+  module.exports = router;
